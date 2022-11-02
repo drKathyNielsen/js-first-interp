e0827a6a86806d89eb50c5e1e05da382a4f3b688
Kathy Nielsen <drkathynielsen@gmail.com>
2022-11-02 10:05:18 -0600

    some change

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const Getnodes = require("./getnodes");
 const getnodes = new Getnodes();
 const program = getnodes.loadfile('./test/sample.js')
 const body = getnodes.parse(program)
-//console.log(body)
+console.log(body)
